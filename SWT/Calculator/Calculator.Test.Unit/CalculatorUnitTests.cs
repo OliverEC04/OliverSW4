91795fca3df311d9920845cf9ca4ddf53ce6be8e
OliverEC <oliverec04@gmail.com>
2024-01-30 13:36:11 +0100

    swt

---
+++ SWT/Calculator/Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Test.Unit
+{
+    internal class CalculatorUnitTests
+    {
+        private Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+
+        [Test]
+        public void Add_PositiveNumbers_SumIsCorrect()
+        {
+            Assert.That(uut.Add(10, 20), Is.EqualTo(30));
+        }
+    }
+}
