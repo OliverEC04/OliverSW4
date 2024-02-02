987b5fcb7912d3a4305b49e9c97c3ab10ce1953e
OliverEC <oliverec04@gmail.com>
2024-02-02 14:12:15 +0100

    fed

---
+++ FED/MauiTodo/MauiTodo/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MauiTodo.Models;
+
+namespace MauiTodo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private string _todoListData = string.Empty;
+        private readonly Database _database;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _database = new Database();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            TodoItem _todoItem = new TodoItem();
+
+            _database.Addtodo(_todoItem);
+        }
+    }
+
+}
