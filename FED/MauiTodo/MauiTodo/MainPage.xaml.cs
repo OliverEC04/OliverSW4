715833e7e961e1873428194174bf5e295419d57d
OliverEC <oliverec04@gmail.com>
2024-02-03 19:24:46 +0100

    suicide

--- FED/MauiTodo/MauiTodo/MainPage.xaml.cs
+++ FED/MauiTodo/MauiTodo/MainPage.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using MauiTodo.Models;
+using System.Collections.ObjectModel;
 
 namespace MauiTodo
 {
     public partial class MainPage : ContentPage
     {
+        ObservableCollection<TodoItem> todoItems;
         int count = 0;
-        private string _todoListData = string.Empty;
         private readonly Database _database;
 
         public MainPage()
@@ -13,13 +14,20 @@ namespace MauiTodo
             InitializeComponent();
 
             _database = new Database();
+
+            TodosCollection.ItemsSource = todoItems;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
             TodoItem _todoItem = new TodoItem();
 
-            _database.Addtodo(_todoItem);
+            _todoItem.Id = 1;
+            _todoItem.Title = "oliver test";
+
+            Console.WriteLine("oliuvrer");
+
+            _ = _database.Addtodo(_todoItem);
         }
     }
 
