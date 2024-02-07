8add41f73027487b9208ec2e68c5d0687e91d09f
OliverEC <oliverec04@gmail.com>
2024-02-07 14:06:43 +0100

    fed

---
+++ FED/SelectImages/SelectImages/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SelectImages;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ FED/SelectImages/SelectImages/Models/ImageInfo.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace SelectImages.Models;
+
+public class ImageInfo
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Path { get; set; }
+    public string Description { get; set; }
+    
+    // var image = await FilePicker.Default.PickAsync(new PickOptions
+    //     {
+    //         PickerTitle = "Pick Image",
+    //         FileTypes = FilePickerFileType.Images
+    //     });
+    //     if (image != null)
+    // {
+    //     _imagePath = image.FullPath.ToString();
+    //     selectedImage.Source = _imagePath;
+    // }
+}
