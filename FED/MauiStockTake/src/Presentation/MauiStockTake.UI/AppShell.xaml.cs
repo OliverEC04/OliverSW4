7ee620ccdbd952a2f3ed3f059be1d6c3d8b1dc1d
OliverEC <oliverec04@gmail.com>
2024-02-16 14:11:09 +0100

    fed

---
+++ FED/MauiStockTake/src/Presentation/MauiStockTake.UI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiStockTake.UI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ FED/MauiStockTake/src/Presentation/MauiStockTake.UI/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using MauiStockTake.UI.Pages;
