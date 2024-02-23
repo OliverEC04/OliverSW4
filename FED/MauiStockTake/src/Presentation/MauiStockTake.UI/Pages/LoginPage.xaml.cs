7653a73801ba679e49f56bf6defd9e4d2faa6823
OliverEC <oliverec04@gmail.com>
2024-02-23 16:43:24 +0100

    fed 7

--- FED/MauiStockTake/src/Presentation/MauiStockTake.UI/Pages/LoginPage.xaml.cs
+++ FED/MauiStockTake/src/Presentation/MauiStockTake.UI/Pages/LoginPage.xaml.cs
@@ -1,9 +1,37 @@
+using MauiStockTake.UI.Services;
+
 namespace MauiStockTake.UI.Pages;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
+    private readonly IAuthService _authService;
+
+    public LoginPage()
+    {
+        InitializeComponent();
+        /* Assign a new instance of the MockAuthService to the field */
+        _authService = new MockAuthService();
+    }
+
+    /* LoginButton_Clicked event handler */
+    private async void LoginButton_Clicked(object sender, EventArgs e)
+    {
+        LoginButton.IsEnabled = false;
+        LoggingIn.IsVisible = true;
+        //Call the LoginAsync method on the IAuthService and assign the result to a temporary variable
+        var loggedIn = await _authService.LoginAsync();
+        LoggingIn.IsVisible = false;
+
+        //If the login is not successful, show a warning to the user
+        //hide the ActivityIndicator, and enable the button.
+        if (!loggedIn)
+        {
+            await App.Current.MainPage.DisplayAlert("Error", "Something went wrong logging you in.Please try again.", "OK");
+            LoginButton.IsEnabled = true;
+        }
+        else
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
 }
