cff36d647610d221664c104caf1e9b953bacf375
OliverEC <oliverec04@gmail.com>
2024-02-29 14:49:48 +0100

    bad

--- BAD/BadAssignment2/SQL.sql
+++ BAD/BadAssignment2/SQL.sql
@@ -1,12 +1,12 @@
 --CREATE DATABASE Bakery
 
--- CREATE TABLE Orderr(
---     FOREIGN KEY (Addres) REFERENCES 
--- );
+CREATE TABLE Orderr(
+    FOREIGN KEY (Addres) REFERENCES 
+);
 
--- CREATE TABLE Addres(
---     Street CHAR(100),
---     Num INT,
---     PostalCode INT,
---     City CHAR(100)
--- );
+CREATE TABLE Addres(
+    Street CHAR(100),
+    Num INT,
+    PostalCode INT,
+    City CHAR(100)
+);
